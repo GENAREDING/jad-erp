34698b0b3d51b40defa9896d088719a054a03903
GENARDING <42216613@qq.com>
2023-10-31 21:29:50 +0800

    3.0 对Hi的提交2

--- src/main/java/com/jad/erp/GENApplication.java
+++ src/main/java/com/jad/erp/GENApplication.java
@@ -3,5 +3,6 @@ package com.jad.erp;
 public class GENApplication {
     public static void main(String[] args) {
         System.out.println("hello 1 --erp!");
+        System.out.println("hello 200 for 3.0");
     }
 }
