fbd3915be852073f237475e0296352017517d887
GENARDING <42216613@qq.com>
2023-10-31 20:22:50 +0800

    Application commit no2 for v2.0

--- src/main/java/com/jad/erp/GENApplication.java
+++ src/main/java/com/jad/erp/GENApplication.java
@@ -3,5 +3,6 @@ package com.jad.erp;
 public class GENApplication {
     public static void main(String[] args) {
         System.out.println("hello 1 --erp!");
+        System.out.println("测试对v2.0的变化");
     }
 }
